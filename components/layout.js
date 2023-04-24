c73e8ca8101da788b909a3f7fad7c3c1f8772fba
JRH89 <thegemlins@gmail.com>
2023-04-23 19:06:05 -0700

    changed oktitle

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Jared R Hooker';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Jared\'s Blog';
 
 export default function Layout({ children, home }) {
     return (
