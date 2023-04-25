bb942f366883ea9e9db75f514f7dc1538734c1a2
JRH89 <thegemlins@gmail.com>
2023-04-25 14:34:29 -0700

    ok

--- components/footer.js
+++ components/footer.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export default function Footer() {
   return (
       <footer>
-          <hr />
+    
       <nav>
         <p>
-        Created by Jared Hooker using Next.js © 2023
+        Created using Next.js © 2023
         </p>
         <Link href="https://jaredsportfolio.netlify.app">← Back to Portfolio</Link>   
       </nav>
