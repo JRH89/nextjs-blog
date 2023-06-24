0dcb7077332263420f1619ac67f56174ba06df3a
JRH89 <thegemlins@gmail.com>
2023-06-23 19:57:47 -0700

    ui

--- components/layout.js
+++ components/layout.js
@@ -1,79 +1,79 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from './layout.module.css';
-import utilStyles from '../styles/utils.module.css';
-import Link from 'next/link';
-import Footer from './footer';
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.css'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import Footer from './footer'
 
-const name = 'Jared R Hooker';
-export const siteTitle = 'Jared\'s Blog';
+const name = 'Jared R Hooker'
+export const siteTitle = 'Jared\'s Blog'
 
 export default function Layout({ children, home }) {
     return (
-<>
-        <div className={styles.container}>
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-                <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
-                <meta name="google" content="notranslate" key="notranslate" />
-                <meta
-                    name="description"
-                    content="Follow along on my coding journey!"
-                />
-                <meta
-                    property="og:image"
-                    content={`https://og-image.vercel.app/${encodeURI(
-                        siteTitle,
-                    )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-                />
-                <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
-            </Head>
-            <header className={styles.header}>
-                {home ? (
-                    <>
-                        <Image
-                            priority
-                            src="/images/profile.jpg"
-                            className={utilStyles.borderCircle}
-                            height={144}
-                            width={144}
-                            alt=""
-                        />
-                        <h1 className={utilStyles.heading2X1}>{name}</h1>
-                    </>
-                ) : (
-                    <>
-                        <Link href="/">
-                            <Image 
+        <>
+            <div className={styles.container}>
+                <Head>
+                    <link rel="icon" href="/icon.jpg" />
+                    <meta name="google" content="nositelinkssearchbox" key="sitelinks" />
+                    <meta name="google" content="notranslate" key="notranslate" />
+                    <meta
+                        name="description"
+                        content="Follow along on my coding journey!"
+                    />
+                    <meta
+                        property="og:image"
+                        content={`https://og-image.vercel.app/${encodeURI(
+                            siteTitle,
+                        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                    />
+                    <meta name="og:title" content={siteTitle} />
+                    <meta name="twitter:card" content="summary_large_image" />
+                </Head>
+                <header className={styles.header}>
+                    {home ? (
+                        <>
+                            <Image
                                 priority
                                 src="/images/profile.jpg"
                                 className={utilStyles.borderCircle}
-                                height={108}
-                                width={108}
+                                height={144}
+                                width={144}
                                 alt=""
                             />
-                        </Link>
-                        <h2 className={utilStyles.headingLg}>
-                            <Link href="/" className={utilStyles.colorInherit}>
-                                {name}
+                            <h1 className={utilStyles.heading2X1}>{name}</h1>
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/">
+                                <Image
+                                    priority
+                                    src="/images/profile.jpg"
+                                    className={utilStyles.borderCircle}
+                                    height={108}
+                                    width={108}
+                                    alt=""
+                                />
                             </Link>
-                        </h2>
-                    </>
+                            <h2 className={utilStyles.headingLg}>
+                                <Link href="/" className={utilStyles.colorInherit}>
+                                    {name}
+                                </Link>
+                            </h2>
+                        </>
+                    )}
+                </header>
+                <main>{children}</main>
+                {!home && (
+                    <div className={styles.backToHome}>
+                        <Link href="/">← Back to home</Link>
+                    </div>
                 )}
-            </header>
-            <main>{children}</main>
-            {!home && (
-                <div className={styles.backToHome}>
-                    <Link href="/">← Back to home</Link>
-                </div>
-            )}
-        
-        </div>
 
-<div classname={global.footer}>
+            </div>
+
+            <div classname={global.footer}>
                 <Footer />
             </div>
-      </>      
-    );
+        </>
+    )
 }
